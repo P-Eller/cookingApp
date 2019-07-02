9db525179959a3bcb46733c446a92d58180d8b82
phillip.eller <phillip.eller@rwth-aachen.de>
2019-07-02 13:34:32 +0200

    first commit

---
+++ djreact/backend/src/recipes/api/views.py
@@ -0,0 +1,17 @@
+
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+
+from recipes.models import Recipe
+from .serializers import RecipeSerializer
+
+
+class RecipeListView(ListAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+
+class RecipeDetailView(RetrieveAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
